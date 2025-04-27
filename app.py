fa76b0052ddb748b3d69361e1ea0a0bdefd480f4
PerlaGCastillo <contactoperlagodinez@gmail.com>
2025-04-26 22:15:06 -0600

    gitignore, creating and getting user routes

--- app.py
+++ app.py
@@ -28,3 +28,21 @@ def test():
     return make_response(jsonify({'message': 'test route'}), 200)
 
 
+@app.route('/users', methods=['POST'])
+def create_user():
+   try:
+        data = request.get_json()
+        new_user = User(username=data['username'], email=data['email'])
+        db.session.add(new_user)
+        db.session.commit()
+        return make_response(jsonify({'message': 'user created'}), 201)
+    except e:
+        return make_response(jsonify({'message': 'error creating user'}), 500)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    try:
+        users = User.query.all()
+        return make_response(jsonify({'users': [user.json() for user in users]}), 200)
+    except e:
+        return make_response(jsonify({'message': 'error getting users'}), 500)
